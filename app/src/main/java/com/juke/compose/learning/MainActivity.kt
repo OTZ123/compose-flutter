aee7650776a398bf1be81ef1f0c053dd34c757b3
Juke <840962517@qq.com>
2023-12-19 15:20:40 +0800

    init flutter shell

--- app/src/main/java/com/juke/compose/learning/MainActivity.kt
+++ app/src/main/java/com/juke/compose/learning/MainActivity.kt
@@ -3,41 +3,36 @@ package com.juke.compose.learning
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.juke.compose.learning.theme.ComposeTheme
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+            DefaultPreview()
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        ComposeTheme {
+            Column {
+                Button(onClick = {
+                    startActivity(
+                        FlutterActivity.createDefaultIntent(this@MainActivity)
+                    )
+                }) {
+                    Text(text = "Go To Flutter")
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeTheme {
-        Greeting("Android")
-    }
-}
